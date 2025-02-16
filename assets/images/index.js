40ec2db94e577670a00ed130fae663ec55102530
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2025-02-16 08:35:46 +0100

    Added icons......

--- assets/images/index.js
+++ assets/images/index.js
@@ -7,14 +7,18 @@ import inactivedocuments from "./bottom-navigation/inactive-document.png"
 import inactivehome from "./bottom-navigation/inactive-home.png"
 import inactiveprofile from "./bottom-navigation/inactive-profile.png"
 
+import carretright from "./icon/carretright.png"
 import edit from "./icon/edit.png"
 import eyeclose from "./icon/eye-close.png"
 import eyeopen from "./icon/eye-open.png"
 import hi from "./icon/hi.png"
 import logout from "./icon/logout.png"
+import legal from "./icon/legal.png"
 import notification from "./icon/notification.png"
+import privacy from "./icon/privacy.png"
+import profile from "./icon/profile.png"
 
 export default { 
     activecases, activedocuments, activehome, activeprofile, inactivecases, inactivedocuments, inactivehome, inactiveprofile, 
-    edit, eyeclose, eyeopen, hi, logout, notification
+    edit, eyeclose, eyeopen, hi, logout, notification, legal, privacy, profile, carretright
 }
